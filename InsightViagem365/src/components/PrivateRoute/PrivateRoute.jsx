0298a93347dfde93b93ce0a4d83c41e5d87f19a0
Roni Rodrigues <108087604+Ronisank@users.noreply.github.com>
2024-08-19 12:50:17 -0300

    Update PrivateRoute.jsx

--- InsightViagem365/src/components/PrivateRoute/PrivateRoute.jsx
+++ InsightViagem365/src/components/PrivateRoute/PrivateRoute.jsx
@@ -5,33 +5,7 @@ export function PrivateRoute() {
     const { user, signOut } = useAuth()
 
     return user ? (
-      // <div className="container-dashboard parent">
-      //   <div className="header">
-      //     <div className="user">
-      //       <User size={28} />
-      //       <span>Olá, {user.nome}</span>
-      //     </div>
-      //     <div className="locations">
-      //       <Link to={"/dashboard"}>
-      //         <Home size={28} className="mappin" />
-      //       </Link>
-      //       <span>Dashboard</span>
-      //     </div>
-      //     <div className="locations">
-      //       <Link to={"/dashboard/locais"}>
-      //         <MapPinPlus size={28} className="mappin" />
-      //       </Link>
-      //       <span>Locais</span>
-      //     </div>
-      //     <div className="exit">
-      //     <button onClick={signOut} className="logout">
-      //       <LogOut size={28} />
-      //     </button>
-      //       <span>Sair</span>
-      //       </div>
-      //   </div>
         <Outlet />
-      // </div>
     ) : (
       <Navigate to="/" />
     );
